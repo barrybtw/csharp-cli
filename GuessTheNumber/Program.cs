ed3074e81dac07764c66e7fc48a1606f841840a5
Nicolai Christensen <nicolaibernhardchristensen@gmail.com>
2024-08-20 13:12:05 +0200

    Starten af 2 CSharp CLIer

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,70 @@
+﻿using static System.Int64;
+
+namespace GuessTheNumber;
+
+public abstract class Program
+{
+    public static void Main()
+    {
+        var isRunning = true;
+        var randNumberInstance = new Random();
+
+        while (isRunning)
+        {
+            var userInput = AskForNumber();
+            
+            Console.WriteLine($"Du har valgt nummeret: {userInput}");
+            var currRand = randNumberInstance.NextInt64(1, userInput);
+            
+            var usersGuess = AskForGuess(userInput);
+            Console.WriteLine(usersGuess == currRand
+                ? $"Du gættede rigtigt! Tallet var {currRand}"
+                : $"Desværre, du gættede ikke rigtigt, det var {currRand}");
+
+            var goAgain = GoAgain();
+            if (!goAgain) isRunning = false;
+        }
+    }
+
+    private static long AskForNumber()
+    {
+        Console.WriteLine("Vælg et positivt tal:");
+        var userInput = Console.ReadLine();
+
+        var parseSucceeded = TryParse(userInput, out var userValue);
+        if (userValue == 0 || parseSucceeded == false)
+        {
+            throw new Exception("Vælg et positivt tal");
+        }
+
+        return userValue;
+    }
+
+    private static long AskForGuess(long maxValue)
+    {
+        Console.WriteLine($"Gæt et nummer mellem 1 og {maxValue}");
+        var userInput = Console.ReadLine();
+
+        var parseSucceeded = TryParse(userInput, out var userGuess);
+
+        if (userGuess == 0 || parseSucceeded == false)
+        {
+            throw new Exception("Vælg et positivt tal");
+        }
+
+        return userGuess;
+    }
+
+    private static bool GoAgain()
+    {
+        Console.WriteLine("Vil du prøve igen? Skriv enten 'ja' eller 'nej'");
+        var userInput = Console.ReadLine();
+
+        if (userInput == null)
+        {
+            throw new Exception("Vælg enten ja eller nej");
+        }
+
+        return "ja".Equals(userInput.ToLower());
+    }
+}
---
+++ OldEnough/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace OldEnough;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var birthday = GetBirthday();
+        var eighteenPlus = DateTime.Now.AddYears(-18);
+
+        var isEighteen = birthday < eighteenPlus;
+        Console.WriteLine(isEighteen ? $"Du er over 18 år gammel." : "Du er ikke over 18 år gammel, fy fy.");
+    }
+
+    private static DateTime GetBirthday()
+    {
+        Console.WriteLine("Hvad er din fødselsdag? Brug format dd-MM-yyyy");
+        var userInput = Console.ReadLine();
+
+        var culture = CultureInfo.GetCultureInfo("da-DK");
+        const DateTimeStyles style = DateTimeStyles.None;
+
+
+        var success = DateTime.TryParseExact(
+            userInput,
+            "dd-MM-yyyy",
+            culture,
+            style,
+            out var birthDay
+        );
+
+        if (!success)
+        {
+            throw new Exception("Datoen blev indtastet forkert");
+        }
+
+        return birthDay;
+    }
+}
